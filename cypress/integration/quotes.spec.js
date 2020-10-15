30896423f2a8bfcbf997db0cba6698d650207591
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-15 18:34:18 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,9 @@
 // write tests here
+describe('Quotes App', () => {
+
+  // schedule somethign to happen before each test
+  // before each test we navigate to http://localhost:1234
+  beforeEach(() => {
+    cy.visit('http://localhost:1234')
+  })
+})
